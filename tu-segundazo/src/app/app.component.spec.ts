ea6f2469829cb5b8acca04b569a4b25b86511572
Oscar Ramirez <o.ramirezb@uniandes.edu.co>
2023-04-29 10:49:59 -0500

    estilos pagina principal

--- tu-segundazo/src/app/app.component.spec.ts
+++ tu-segundazo/src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('tu-segundazo');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tu-segundazo app is running!');
-  });
+
 });
