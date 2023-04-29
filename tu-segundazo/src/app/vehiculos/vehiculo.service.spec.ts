4755baacb384e43f7ed7e174700685cadb71b31b
Oscar Ramirez <o.ramirezb@uniandes.edu.co>
2023-04-29 10:31:30 -0500

    Servicio vehiculos

--- tu-segundazo/src/app/vehiculos/vehiculo.service.spec.ts
+++ tu-segundazo/src/app/vehiculos/vehiculo.service.spec.ts
@@ -1,16 +1,81 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { VehiculoService } from './vehiculo.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Vehiculo } from './vehiculo';
+import { faker } from '@faker-js/faker';
 
 describe('Service: Vehiculo', () => {
+  let service: VehiculoService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [VehiculoService]
+    });
+
+    service = TestBed.inject(VehiculoService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getVehuculos', () => {
+    it('should return an Observable<Vehiculo[]>', () => {
+      const dummyVehiculos: Vehiculo[] = [
+        new Vehiculo(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.image.imageUrl()
+        ),
+        new Vehiculo(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.image.imageUrl()
+        ),
+        new Vehiculo(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.image.imageUrl()
+        ),
+
+      ];
+
+      service.getVehiculos().subscribe((Vehiculo) => {
+        expect(Vehiculo.length).toBe(3);
+        expect(Vehiculo).toEqual(dummyVehiculos);
+      });
+
+
+
+      const req = httpMock.expectOne('https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json');
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyVehiculos);
     });
   });
 
-  it('should ...', inject([VehiculoService], (service: VehiculoService) => {
-    expect(service).toBeTruthy();
-  }));
+
 });
--- tu-segundazo/src/app/vehiculos/vehiculo.service.ts
+++ tu-segundazo/src/app/vehiculos/vehiculo.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Vehiculo } from './vehiculo';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VehiculoService {
+  private apiUrl: string =
+    'https://gist.githubusercontent.com/josejbocanegra/17bb8c76405e43655d551a90800c8a81/raw/d41b4acc3457e51e7533fad6d5e9925ee9676457/202212_MISW4104_Grupo1.json';
 
-constructor() { }
+  constructor(private http: HttpClient) {}
 
+  getVehiculos(): Observable<Vehiculo[]> {
+    return this.http.get<Vehiculo[]>(this.apiUrl);
+  }
 }
